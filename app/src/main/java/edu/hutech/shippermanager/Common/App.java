0c3e1ab94e09a58c74196e1b4a7be4bc555630e0
Hoang Minh Nhat <jeremy.hutech@gmail.com>
2016-10-16 16:20:32 +0700

    Viet Code LoginActivity

---
+++ app/src/main/java/edu/hutech/shippermanager/Common/App.java
@@ -0,0 +1,21 @@
+package edu.hutech.shippermanager.Common;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by hienl on 10/9/2016.
+ */
+
+public class App extends Application {
+    private static Context mContext;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+
+    public static Context getContext(){
+        return mContext;
+    }
+}
