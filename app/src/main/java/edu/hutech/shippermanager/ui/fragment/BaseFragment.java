595049b5a3da186f76950a498e0e03321d4c3d5a
= <=>
2016-11-04 19:22:23 +0700

    them BaseFragment

---
+++ app/src/main/java/edu/hutech/shippermanager/ui/fragment/BaseFragment.java
@@ -0,0 +1,10 @@
+package edu.hutech.shippermanager.ui.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by hienl on 11/4/2016.
+ */
+
+public class BaseFragment extends Fragment {
+}
