4a1cf0b5746e7cc2007e3f2e2425998a47e7d199
= <=>
2016-11-04 18:16:38 +0700

    init baseActivity

--- app/src/main/java/edu/hutech/shippermanager/ui/activity/BaseActivity.java
+++ app/src/main/java/edu/hutech/shippermanager/ui/activity/BaseActivity.java
@@ -1,11 +1,21 @@
 package edu.hutech.shippermanager.ui.activity;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by hienl on 11/4/2016.
  */
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+    abstract int getContentView();
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentView());
+        ButterKnife.bind(this);
+    }
 }
---
+++ app/src/main/java/edu/hutech/shippermanager/ui/activity/BaseActivityAuthorization.java
@@ -0,0 +1,45 @@
+package edu.hutech.shippermanager.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * Created by hienl on 11/4/2016.
+ */
+
+public abstract class BaseActivityAuthorization extends BaseActivity implements FirebaseAuth.AuthStateListener {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        onAuthChanged(firebaseAuth);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(this);
+    }
+
+    protected FirebaseAuth getFireBaseAuth() {
+        return mAuth;
+    }
+
+    abstract void onAuthChanged(FirebaseAuth firebaseAuth);
+}
+
