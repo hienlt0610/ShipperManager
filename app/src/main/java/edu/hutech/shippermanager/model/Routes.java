77dbaf98fd760e64eb5717201ff72b59a65532be
Hoang Minh Nhat <jeremy.hutech@gmail.com>
2016-11-13 23:08:36 +0700

    update

---
+++ app/src/main/java/edu/hutech/shippermanager/model/Routes.java
@@ -0,0 +1,8 @@
+package edu.hutech.shippermanager.model;
+
+/**
+ * Created by jerem on 12/11/2016.
+ */
+
+public class Routes {
+}
---
+++ app/src/main/java/edu/hutech/shippermanager/service/DirectionFinder.java
@@ -0,0 +1,8 @@
+package edu.hutech.shippermanager.service;
+
+/**
+ * Created by jerem on 12/11/2016.
+ */
+
+public class DirectionFinder {
+}
