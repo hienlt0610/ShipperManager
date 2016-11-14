afad13a2c46de5055af634b74218a8d63a88e316
Hoang Minh Nhat <jeremy.hutech@gmail.com>
2016-11-15 02:14:27 +0700

    update ve polyline tu vi tri hien tai toi dia diem can tim

--- app/src/main/java/edu/hutech/shippermanager/utils/LocationUtils.java
+++ app/src/main/java/edu/hutech/shippermanager/utils/LocationUtils.java
@@ -1,10 +1,18 @@
 package edu.hutech.shippermanager.utils;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
 
 /**
  * Created by hienl on 11/5/2016.
@@ -30,7 +38,7 @@ public class LocationUtils {
      * @param positiveLabel     The positive button text
      * @param negativeLabel     The negative button text
      */
-    public static void askEnableProviders(final Context context, String message, String positiveLabel, String negativeLabel){
+    public static void askEnableProviders(final Context context, String message, String positiveLabel, String negativeLabel) {
         new AlertDialog.Builder(context)
                 .setMessage(message)
                 .setCancelable(false)
@@ -47,5 +55,34 @@ public class LocationUtils {
                     }
                 })
                 .show();
+    }
+
+    public static LatLng getLatLng(Location location) {
+        if (location == null) {
+            return null;
+        }
+        return new LatLng(location.getLatitude(), location.getLongitude());
+    }
+
+    public static Location getLastKnownLoaction(boolean enabledProvidersOnly, Context context) {
+        LocationManager manager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        Location utilLocation = null;
+        List<String> providers = manager.getProviders(enabledProvidersOnly);
+        for (String provider : providers) {
+
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            utilLocation = manager.getLastKnownLocation(provider);
+            if(utilLocation != null) return utilLocation;
+        }
+        return null;
     }
 }
