1f87a67ce8c1a4bdef0e5d9e7a6b6c3906e4a886
Hoang Minh Nhat <jeremy.hutech@gmail.com>
2016-11-22 01:03:12 +0700

    them popmenu fragmenthome

--- app/src/main/java/edu/hutech/shippermanager/ui/fragment/HomeFragment.java
+++ app/src/main/java/edu/hutech/shippermanager/ui/fragment/HomeFragment.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,7 +26,6 @@ import butterknife.BindView;
 import edu.hutech.shippermanager.R;
 import edu.hutech.shippermanager.adapter.OrderAdapter;
 import edu.hutech.shippermanager.common.FirebaseConfig;
-import edu.hutech.shippermanager.common.L;
 import edu.hutech.shippermanager.model.Order;
 import edu.hutech.shippermanager.model.User;
 import edu.hutech.shippermanager.ui.activity.OrderDetailActivity;
@@ -45,6 +46,7 @@ public class HomeFragment extends BaseFragment implements AdapterView.OnItemClic
     ArrayList<Order> orders;
     OrderAdapter adapter = null;
     FirebaseUser fUser;
+    DatabaseReference root;
 
     public static HomeFragment newInstance(String userID) {
         HomeFragment fragment = new HomeFragment();
@@ -61,14 +63,14 @@ public class HomeFragment extends BaseFragment implements AdapterView.OnItemClic
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 //        final DatabaseReference root = FirebaseDatabase.getInstance().getReference(FirebaseConfig.USERS_CHILD);
-        final DatabaseReference root = FirebaseDatabase.getInstance().getReference();
         fUser = FirebaseAuth.getInstance().getCurrentUser();
+        root = FirebaseDatabase.getInstance().getReference();
 //        DatabaseReference demo = FirebaseDatabase.getInstance().getReference();
 //        Order order = new Order("38 nguyen Xi", 10.801147, 106.710624, 187, true, "Nhat Hoang", "Hien Le", fUser.getUid().toString());
 //        demo.child("orders").push().setValue(order);
 
         orders = new ArrayList<Order>();
-        adapter = new OrderAdapter(getActivity(),R.layout.item_order,orders);
+        adapter = new OrderAdapter(getActivity(), R.layout.item_order, orders);
         lvOrders.setAdapter(adapter);
         if (fUser != null) {
             root.child(FirebaseConfig.USERS_CHILD).child(fUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -92,7 +94,7 @@ public class HomeFragment extends BaseFragment implements AdapterView.OnItemClic
                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                     Order order = dataSnapshot.getValue(Order.class);
                     order.setOrderID(dataSnapshot.getKey());
-                    if(order == null) return;
+                    if (order == null) return;
                     if (order.getUserID().equals(fUser.getUid())) {
                         //orders.add("Địa chỉ: " + order.getReceiver().getAddress() + "\nTình trạng: " + order.isStatus());
                         //adapter.notifyDataSetChanged();
@@ -134,11 +136,34 @@ public class HomeFragment extends BaseFragment implements AdapterView.OnItemClic
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
-        Order order = orders.get(pos);
-        L.Toast(order.getOrderID());
-        Intent intent = new Intent(getActivity(), OrderDetailActivity.class);
-        intent.putExtra("orderID", order.getOrderID());
-        startActivity(intent);
+        final Order order = orders.get(pos);
+
+        //Creating the instance of PopupMenu
+        PopupMenu popup = new PopupMenu(getActivity(), view);
+        //Inflating the Popup using xml file
+        popup.getMenuInflater().inflate(R.menu.poupup_menu, popup.getMenu());
+
+        //registering popup with OnMenuItemClickListener
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.menu_act_view_info:
+                        Intent intent = new Intent(getActivity(), OrderDetailActivity.class);
+                        intent.putExtra("orderID", order.getOrderID());
+                        startActivity(intent);
+                        break;
+                    case R.id.menu_act_run_order:
+                        DatabaseReference uLocationItem = root.child("user_location").child(fUser.getUid());
+                        uLocationItem.child("orders").setValue(order);
+
+                        break;
+                }
+                return true;
+            }
+        });
+
+        popup.show();//showing popup menu
     }
 
 
