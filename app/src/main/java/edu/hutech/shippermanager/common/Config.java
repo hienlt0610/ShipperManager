0b5e3ad5ead4539fd0289eff95dfb7e117dc4b54
= <hienlt0610@gmail.com>
2016-11-22 23:57:07 +0700

    add library direction map

---
+++ app/src/main/java/edu/hutech/shippermanager/common/Config.java
@@ -0,0 +1,9 @@
+package edu.hutech.shippermanager.common;
+
+/**
+ * Created by hienl on 11/22/2016.
+ */
+
+public class Config {
+    public static final String GOOGLE_MAP_API_KEY = "AIzaSyCsDw_v8GIsJcDVWHijIvDVZYc5wjZjIbc";
+}
